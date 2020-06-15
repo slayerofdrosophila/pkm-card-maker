38b5a8c05140fc39db8dab81da18a592ef9baaf3
Leon van der Noll <leonvandernoll@gmail.com>
2020-06-15 09:40:07 +0200

    Updates

---
+++ precache-manifest.13f3f2964ca763b25e0de07b774acd6a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc7b204da4b38fbb9d3d90990d4e536",
+    "url": "/redux-pkm-card-maker/index.html"
+  },
+  {
+    "revision": "62ced0e4a8833ea2ed10",
+    "url": "/redux-pkm-card-maker/static/css/main.8296ad86.chunk.css"
+  },
+  {
+    "revision": "1001dedc09fa5e7e4c45",
+    "url": "/redux-pkm-card-maker/static/js/2.96ccbc42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-pkm-card-maker/static/js/2.96ccbc42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ced0e4a8833ea2ed10",
+    "url": "/redux-pkm-card-maker/static/js/main.258e16a7.chunk.js"
+  },
+  {
+    "revision": "6fc9060c6436a5326d57",
+    "url": "/redux-pkm-card-maker/static/js/runtime-main.cb6d4451.js"
+  },
+  {
+    "revision": "6dbaa4c300a2f94ade3fb8ba2b4f6c97",
+    "url": "/redux-pkm-card-maker/static/media/FrutigerLTStdBoldItalic.6dbaa4c3.ttf"
+  },
+  {
+    "revision": "6457161a2bd1197bca298f423ddeb76a",
+    "url": "/redux-pkm-card-maker/static/media/FuturaStdBoldOblique.6457161a.otf"
+  },
+  {
+    "revision": "2e40a82b6659fa86f1537a28f3569200",
+    "url": "/redux-pkm-card-maker/static/media/GillSans.2e40a82b.otf"
+  },
+  {
+    "revision": "3c256200e33b0a45ef0f318b944dbc14",
+    "url": "/redux-pkm-card-maker/static/media/GillSansStd.3c256200.otf"
+  },
+  {
+    "revision": "687a5d950fa140d3095fb9fe5e59f6a8",
+    "url": "/redux-pkm-card-maker/static/media/ptcg-webfont.687a5d95.woff2"
+  }
+]);
