f9e1b50b732cd18be9016e450af4e3e0dfc0e82b
Leon van der Noll <leonvandernoll@gmail.com>
2020-06-15 11:11:22 +0200

    Updates

---
+++ precache-manifest.f6c2b1eb261ed77ab3890cf5aa80946f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53fbfc58a847a7dee91f906b010972f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f1e94da16a938fbf58ab",
+    "url": "./static/css/main.f6aaec65.chunk.css"
+  },
+  {
+    "revision": "7bf3c0753233d2b848ce",
+    "url": "./static/js/2.58f95280.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.58f95280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e94da16a938fbf58ab",
+    "url": "./static/js/main.d755a471.chunk.js"
+  },
+  {
+    "revision": "b19cca16504173633319",
+    "url": "./static/js/runtime-main.c59afe09.js"
+  },
+  {
+    "revision": "6dbaa4c300a2f94ade3fb8ba2b4f6c97",
+    "url": "./static/media/FrutigerLTStdBoldItalic.6dbaa4c3.ttf"
+  },
+  {
+    "revision": "6457161a2bd1197bca298f423ddeb76a",
+    "url": "./static/media/FuturaStdBoldOblique.6457161a.otf"
+  },
+  {
+    "revision": "2e40a82b6659fa86f1537a28f3569200",
+    "url": "./static/media/GillSans.2e40a82b.otf"
+  },
+  {
+    "revision": "3c256200e33b0a45ef0f318b944dbc14",
+    "url": "./static/media/GillSansStd.3c256200.otf"
+  },
+  {
+    "revision": "687a5d950fa140d3095fb9fe5e59f6a8",
+    "url": "./static/media/ptcg-webfont.687a5d95.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/redux-pkm-card-maker/precache-manifest.ef02338275036889de34ff99e1ca8c92.js"
+  "./precache-manifest.f6c2b1eb261ed77ab3890cf5aa80946f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux-pkm-card-maker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
